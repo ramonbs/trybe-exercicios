6119753bfff1dae62d0b720136e2fc2210ccf0a3
Ramon Giovani <ramongiovanii12@gmail.com>
2022-10-07 12:24:58 -0300

    adicionando secao 6

---
+++ fundamentos/secao-6-html-css-flexbox/dia-01-forms/script.js
@@ -0,0 +1,59 @@
+function preventButtonSubmit() {
+  const button = document.querySelector("#submit");
+  //validate if number of characters is between 10 and 40 in the input name and input email
+  const inputName = document.querySelector("#input-name");
+  const inputEmail = document.querySelector("#input-email");
+  if (inputName.value.length < 10 || inputName.value.length > 40) {
+    alert("Name must be between 10 and 40 characters");
+    button.addEventListener("click", (event) => {
+      event.preventDefault();
+    });
+  }
+  if (inputEmail.value.length < 10 || inputEmail.value.length > 50) {
+    alert("Email must be between 10 and 50 characters");
+    button.addEventListener("click", (event) => {
+      event.preventDefault();
+    });
+  }
+}
+
+function resetButton() {
+  const button = document.querySelector("#reset");
+  button.addEventListener("click", (event) => {
+    event.reload();
+  });
+}
+
+function createLabel() {
+  const input = document.querySelector("#agree2");
+  const sectionLabel = document.querySelector("#img");
+  input.addEventListener("click", (event) => {
+    if (event.target.checked) {
+      const label = document.createElement("label");
+      label.setAttribute("for", "agree2");
+      label.textContent = "Eu aceito os termos de uso: ";
+      sectionLabel.appendChild(label);
+    } else {
+      sectionLabel.removeChild(sectionLabel.lastChild);
+    }
+  });
+}
+
+function createInputImage() {
+  const input = document.querySelector("#agree2");
+  const sectionImg = document.querySelector("#img");
+  input.addEventListener("click", (event) => {
+    if (event.target.checked) {
+      const image = document.createElement("input");
+      image.setAttribute("type", "file");
+      sectionImg.appendChild(image);
+    } else {
+      sectionImg.removeChild(sectionImg.lastChild);
+    }
+  });
+}
+
+preventButtonSubmit();
+createLabel();
+createInputImage();
+resetButton();
